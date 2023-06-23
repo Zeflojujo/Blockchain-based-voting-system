4a2ab3b95e18a78e54d46f28c3ce74d6f4f5d68b
josephat <zeflojujo@gmail.com>
2023-06-23 22:05:03 +0300

    create AuthProvider page

--- src/components/Admin/home.js
+++ src/components/Admin/home.js
@@ -27,7 +27,7 @@ const SignInComponent = () => {
   return (
     <div className="container" id="main">
       <div className="sign-up">
-        <form action="">
+        <form action="" className='form-home'>
           <h1>Sign In</h1>
           <p>For Admin Only</p>
           <div className="social-container">
@@ -42,7 +42,7 @@ const SignInComponent = () => {
             </a>
           </div>
           <p>Click below button to Sign In as an Admin</p>
-          <button id="admin" onClick={handleAdminLogin}>sign In</button>
+          <button className='home-button' id="admin" onClick={handleAdminLogin}>sign In</button>
         </form>
       </div>
 
@@ -65,7 +65,7 @@ const SignInComponent = () => {
           <small>
             Don't you have an account? <a href="/user/regVoter" style={{color: "#1d9b39"}}>Create Account</a>
           </small>
-          <button id="voter" onClick={handleVoterLogin}>sign in</button>
+          <button className='home-button' id="voter" onClick={handleVoterLogin}>sign in</button>
         </form>
       </div>
 
@@ -74,13 +74,13 @@ const SignInComponent = () => {
           <div className="overlay-left">
             <h1>Welcome Back!</h1>
             <p>Secure platform based on a new and rapid emerging technology of Blockchain</p>
-            <button onClick={handleSignInClick} style={{border: "1px solid #fff", backgroundColor: "transparent"}}>Voter</button>
+            <button className='home-button' onClick={handleSignInClick} style={{border: "1px solid #fff", backgroundColor: "transparent"}}>Voter</button>
           </div>
 
           <div className="overlay-right">
             <h1>Hello</h1>
             <p>Welcome to the Blockchain Based Electronic Voting system for Higher Learning Institutions.</p>
-            <button onClick={handleSignUpClick} style={{border: "1px solid #fff"}}>Admin</button>
+            <button className='home-button' onClick={handleSignUpClick} style={{border: "1px solid #fff"}}>Admin</button>
           </div>
         </div>
       </div>
